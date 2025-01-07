b18659a4032269d247c1f463f61c3031fe02b655
Pierre Grunnagel <pierre.grunnagel@mai.etat.lu>
2025-01-07 11:34:58 +0100

    check_stuff update

--- check_stuff.js
+++ check_stuff.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        check_stuff
-// @version     0.1
+// @version     0.2
 // @namespace   https://github.com/Grunnpi/MonkeyStuff
 // @author      Pierre
 // @description This script will automagically blah blah blah
@@ -15,19 +15,31 @@
         return document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
     }
 
-    // XPath de l'élément cible
-    var xpath = '//*[@id="__section0-innerGrid"]';
+    // Fonction pour afficher la popup avec la valeur de l'attribut class
+    function showClassAttribute() {
+        var xpath = '//*[@id="__section0-innerGrid"]';
+        var element = getElementByXPath(xpath);
 
-    // Obtenir l'élément
-    var element = getElementByXPath(xpath);
-
-    if (element) {
-        // Récupérer la valeur de l'attribut class
-        var classAttribute = element.getAttribute('class');
-
-        // Afficher la popup avec la valeur de l'attribut class
-        alert('Class attribute: ' + classAttribute);
-    } else {
-        alert('Element not found.');
+        if (element) {
+            var classAttribute = element.getAttribute('class');
+            alert('Class attribute: ' + classAttribute);
+        } else {
+            console.log('Element not found.');
+        }
     }
+
+    // Appliquer la fonction initialement
+    showClassAttribute();
+
+    // Utiliser un observateur de mutations pour détecter les changements dans le DOM
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.addedNodes.length || mutation.removedNodes.length) {
+                showClassAttribute();
+            }
+        });
+    });
+
+    // Configurer l'observateur pour surveiller les changements dans le body
+    observer.observe(document.body, { childList: true, subtree: true });
 })();
