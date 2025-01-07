eb30a3d5793923e4f3bf854c405f6b2108c2f8fd
Pierre Grunnagel <pierre.grunnagel@mai.etat.lu>
2025-01-07 09:16:03 +0100

    simple script for initial test

---
+++ helloworld.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        helloworld
+// @version     0.1
+// @namespace   https://github.com/Grunnpi/MonkeyStuff
+// @author      Pierre
+// @description This script will automagically blah blah blah
+// @include *
+// ==/UserScript==
+alert('Hello world!');
