f748035ee4306873d5d9da4973f06c8ab9fb1597
Pierre Grunnagel <pierre.grunnagel@mai.etat.lu>
2025-01-07 11:06:54 +0100

    another sap stuff

---
+++ desktop_sap_ui.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        desktop_sap_ui.js
+// @version     0.1
+// @namespace   https://github.com/Grunnpi/MonkeyStuff
+// @author      Pierre
+// @description  Force SAP JavaScript to return desktop mode
+// @match        *://*/*
+// @run-at       document-end
+// ==/UserScript==
+(function() {
+    'use strict';
+
+    // Fonction pour surcharger les attributs
+    function overrideSAPUIAttributes() {
+        var sapUiCore = document.querySelector('[data-sap-ui-browser]');
+        if (sapUiCore) {
+            sapUiCore.setAttribute('data-sap-ui-browser', 'cr90'); // Exemple pour Chrome 90 sur desktop
+            sapUiCore.setAttribute('data-sap-ui-os', 'Windows10'); // Exemple pour Windows 10
+            console.log('Attributs SAP UI surchargés :', sapUiCore);
+        } else {
+            console.log('Élément SAP UI non trouvé.');
+        }
+    }
+
+    // Exécuter la fonction après le chargement de la page
+    overrideSAPUIAttributes();
+})();
